8b2f2e2f2a5ba43e3ef283f66b97197df55c72bc
zhitianbai <zhitianbai@163.com>
2022-06-21 11:17:49 +0800

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by 支天白 on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
