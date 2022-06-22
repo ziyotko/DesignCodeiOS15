f333882b598f30c803be0ab08480512753e03407
zhitianbai <zhitianbai@163.com>
2022-06-22 10:21:04 +0800

    init

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            Text("SwiftUI for iOS15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary,.primary.opacity(0.5)], startPoint:.topLeading, endPoint: .bottomTrailing))
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth:.infinity,alignment: .leading)
+                .foregroundColor(.secondary)
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical,20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial)
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal,20)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height:230)
+                .offset(x:32,y: -80)
+        )
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+            
+            
     }
 }
+
